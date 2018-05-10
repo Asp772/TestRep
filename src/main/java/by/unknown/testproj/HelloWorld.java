ea3fd6a47bb9e79523b120330faa081897c6e4b7
Siarhei Kamarou <Kamarou_S@IBA>
2018-05-10 14:14:26 +0300

    check jenkins

--- src/main/java/by/unknown/testproj/HelloWorld.java
+++ src/main/java/by/unknown/testproj/HelloWorld.java
@@ -2,6 +2,7 @@ package by.unknown.testproj;
 public class HelloWorld{
 public static void main(String[]args){
 System.out.println("Hello World");
+System.out.println("Second line");
 /* commit for changing my branch*/
 }
 }
